3d47be9d904b27d68beb0b4b10649a6e963b92d3
domlander <lander45@hotmail.co.uk>
2022-02-16 06:43:41 +0000

    Reuse existing prisma client in nextauth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,9 +2,7 @@ import NextAuth from "next-auth";
 import EmailProvider from "next-auth/providers/email";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import prisma from "prisma/client";
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
