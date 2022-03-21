699378bae596c1f390608d7ae4285a1140e18f4a
domlander <lander45@hotmail.co.uk>
2022-03-21 16:46:55 +0000

    Add fetchLatestScores API, that will update scores based on the latest data from the FPL API.

---
+++ pages/api/fetchLatestScores.ts
@@ -0,0 +1,113 @@
+/* eslint-disable camelcase */
+import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
+import { withSentry } from "@sentry/nextjs";
+import prisma from "prisma/client";
+import { getFixturesFromApiForGameweek } from "utils/fplApi";
+import Fixture from "src/types/Fixture";
+import { calculateCurrentGameweek } from "../../utils/calculateCurrentGameweek";
+
+const hasScoreChanged = (
+  oldScore: [number | null, number | null],
+  newScore: [number | null, number | null]
+) => oldScore[0] !== newScore[0] || oldScore[1] !== newScore[1];
+
+const isGameLiveOrRecentlyFinished = (kickoff: Date) => {
+  const now = new Date();
+  const timeIn150Minutes = new Date();
+  timeIn150Minutes.setMinutes(timeIn150Minutes.getMinutes() + 150);
+
+  return kickoff > now && kickoff < timeIn150Minutes;
+};
+
+/*
+  Fetches the up-to-date results of a match using the FPL API.
+  Makes no changes if there is not a live game (kickoff between now and 150 minutes from now)
+  If a goal has been scored, calls the updateFixtureResults API to update the
+  fixture score and subsequent predictions score for all predictions.
+*/
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const secret = req.query.secret as string;
+  const session = await getSession({ req });
+
+  if (!process.env.ADMIN_EMAIL)
+    return res
+      .status(500)
+      .send("Please ensure the ADMIN_EMAIL environment variable is set.");
+
+  if (!process.env.ACTIONS_SECRET)
+    return res
+      .status(500)
+      .send("Please ensure the ACTIONS_SECRET environment variable is set.");
+
+  if (
+    session?.user?.email !== process.env.ADMIN_EMAIL &&
+    secret !== process.env.ACTIONS_SECRET
+  ) {
+    return res
+      .status(401)
+      .send("You are not authorised to perform this action.");
+  }
+
+  const fixtures = await prisma.fixture.findMany();
+  const currentGameweek = calculateCurrentGameweek(fixtures);
+  const fixturesFromDb = fixtures.filter(
+    ({ gameweek }) => gameweek === currentGameweek
+  );
+
+  const liveFixtures = fixturesFromDb.filter(({ kickoff }) =>
+    isGameLiveOrRecentlyFinished(kickoff)
+  );
+  if (!liveFixtures?.length) {
+    return res.status(200).send("No live games found");
+  }
+
+  const fixturesFromApi = await getFixturesFromApiForGameweek(currentGameweek);
+
+  const fixturesToUpdate = liveFixtures.reduce((acc, fixture) => {
+    const matchingFixtureFromApi = fixturesFromApi.find(
+      // If two teams playing against each other twice in a gameweek becomes a thing, we can compare on the day as well
+      (x) => x.homeTeam === fixture.homeTeam && x.awayTeam === fixture.awayTeam
+    );
+    if (!matchingFixtureFromApi) {
+      return acc;
+    }
+
+    if (
+      hasScoreChanged(
+        [fixture.homeGoals, fixture.awayGoals],
+        [matchingFixtureFromApi.homeGoals, matchingFixtureFromApi.awayGoals]
+      )
+    ) {
+      acc.push({
+        ...fixture,
+        homeGoals: matchingFixtureFromApi?.homeGoals ?? null,
+        awayGoals: matchingFixtureFromApi?.awayGoals ?? null,
+      });
+    }
+
+    return acc;
+  }, [] as Fixture[]);
+
+  if (!fixturesToUpdate?.length) {
+    return res
+      .status(200)
+      .json("No recent goals have been scored. No scores to update.");
+  }
+
+  fetch(
+    `${process.env.NEXTAUTH_URL}/api/updateFixtureResults?secret=${process.env.ACTIONS_SECRET}`,
+    {
+      method: "post",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ scores: fixturesToUpdate }),
+    }
+  );
+
+  return res.status(200).json({ fixtures: fixturesToUpdate });
+};
+
+export default withSentry(handler);
