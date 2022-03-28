cd5cdee972896618838e6c2ef015dfa70e3d38f0
domlander <lander45@hotmail.co.uk>
2022-03-28 17:23:22 +0100

    Add a test to check the heading on the homepage

---
+++ __tests__/pages/home/index.test.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { SessionProvider } from "next-auth/react";
+import "@testing-library/jest-dom/extend-expect";
+import { MockedProvider, MockedResponse } from "@apollo/client/testing";
+import Home from "pages/index";
+import { USER_LEAGUES_QUERY } from "apollo/queries";
+
+const mocks: MockedResponse[] = [
+  {
+    request: {
+      query: USER_LEAGUES_QUERY,
+    },
+    result: {
+      data: {
+        user: {
+          leagues: [],
+        },
+      },
+    },
+  },
+];
+
+const sessionMock = {
+  expires: "1",
+  user: { id: "abc123", username: "Test User" },
+};
+
+describe("Home", () => {
+  it("renders a heading", () => {
+    render(
+      <SessionProvider session={sessionMock}>
+        <MockedProvider mocks={mocks} addTypename={false}>
+          <Home weekId={1} fixtures={[]} />
+        </MockedProvider>
+      </SessionProvider>
+    );
+
+    expect(screen.getByRole("heading")).toHaveTextContent("This week");
+  });
+});
