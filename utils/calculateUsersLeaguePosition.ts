350ae6bb679389e4694a031f6dc38c13da49d67c
Dominik Lander <dominik.lander@guardian.co.uk>
2023-03-04 06:14:15 +0000

    Fix bug: Current position in leagues always shows 1st (attempt 3)

--- utils/calculateUsersLeaguePosition.ts
+++ utils/calculateUsersLeaguePosition.ts
@@ -1,6 +1,9 @@
 import User from "src/types/User";
 
-type PartialUser = Pick<User, "id" | "totalPoints">;
+type PartialUser = {
+  id: User["id"];
+  totalPoints: number;
+};
 
 const calculateUsersLeaguePosition = (
   users: PartialUser[],
@@ -8,7 +11,9 @@ const calculateUsersLeaguePosition = (
 ): number | null => {
   const usersScore = users.find(({ id }) => id === thisUserId)?.totalPoints;
 
-  const position = users.findIndex((user) => user.totalPoints === usersScore);
+  const position = users
+    .sort((a, b) => b.totalPoints - a.totalPoints)
+    .findIndex((user) => user.totalPoints === usersScore);
   if (position === -1) {
     return null;
   }
