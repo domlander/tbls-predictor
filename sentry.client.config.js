3d4dfe64d280a1d8cbb5fa3035768a168e64be01
Dominik Lander <dominik.lander@guardian.co.uk>
2023-11-05 10:40:39 +0000

    Add use client to layout

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -15,8 +15,6 @@ Sentry.init({
 
   environment: process.env.ENVIRONMENT,
 
-  // ...
-
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
   // that it will also get attached to your source maps
