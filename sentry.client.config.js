e26001d83f2cdc2cd88d0bb6bd8b3e5bda465627
Dominik Lander <dominik.lander@guardian.co.uk>
2023-11-05 10:23:25 +0000

    Update Sentry config

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -8,11 +8,9 @@ Sentry.init({
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
   // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
+  tracesSampleRate: 0.5,
 
-  // Capture Replay for 10% of all sessions,
-  // plus for 100% of sessions with an error
-  replaysSessionSampleRate: 0.1,
+  replaysSessionSampleRate: 0.01,
   replaysOnErrorSampleRate: 1.0,
 
   environment: process.env.ENVIRONMENT,
